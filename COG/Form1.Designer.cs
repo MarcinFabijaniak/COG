2abd2b71edf41a6428f1317fe584c2465b13efd8
MarcinFabijaniak <31275949+MarcinFabijaniak@users.noreply.github.com>
2018-03-12 08:39:10 +0000

    Add project files.

---
+++ COG/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace COG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openCOGFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1090, 264);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1117, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openCOGFileToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openCOGFileToolStripMenuItem
+            // 
+            this.openCOGFileToolStripMenuItem.Name = "openCOGFileToolStripMenuItem";
+            this.openCOGFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openCOGFileToolStripMenuItem.Text = "Open COG File";
+            this.openCOGFileToolStripMenuItem.Click += new System.EventHandler(this.openCOGFileToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(160, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(945, 31);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COG file read";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(243, 356);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(298, 40);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(17, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 40);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Create COGs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(560, 361);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(330, 31);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 490);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openCOGFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ COG/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using Tekla.Structures.Model;
+using TSM=Tekla.Structures.Model;
+
+namespace COG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string fileToRead;
+        DataTable dataTable = new DataTable("COG");
+
+        private static void newObject(string fromModelCUP, double fromModelCOG_X, double fromModelCOG_Y, double fromModelCOG_Z)
+        {
+            TSM.Beam newObject = new TSM.Beam();
+
+            newObject.Name = "COG for: " + fromModelCUP;
+            newObject.Profile.ProfileString = "D100";
+            newObject.Material.MaterialString = "Z35";
+            newObject.Class = "1000";
+            newObject.PartNumber.Prefix = "cg";
+            newObject.AssemblyNumber.Prefix = "CG";
+            newObject.StartPoint.X = fromModelCOG_X;
+            newObject.StartPoint.Y = fromModelCOG_Y;
+            newObject.StartPoint.Z = fromModelCOG_Z;
+            newObject.EndPoint.X = fromModelCOG_X + 10000;
+            newObject.EndPoint.Y = fromModelCOG_Y;
+            newObject.EndPoint.Z = fromModelCOG_Z;
+            newObject.Position.Rotation = Position.RotationEnum.TOP;
+            newObject.Position.Plane = Position.PlaneEnum.MIDDLE;
+            newObject.Position.Depth = Position.DepthEnum.MIDDLE;
+
+            newObject.Insert();
+        }        
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void openCOGFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Model MyModel = new Model();
+
+            // Always remember to check that you really have working connection
+            if (MyModel.GetConnectionStatus())
+            {
+                //opens file dialog and lets user select file to open then reds file name
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.InitialDirectory = "d:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                int size = -1;
+                if (openFileDialog.ShowDialog() == DialogResult.OK) // Test result.
+                {
+                    try
+                    {
+                        fileToRead = openFileDialog.FileName;
+                        string text = File.ReadAllText(fileToRead);
+                        size = text.Length;
+                        textBox1.Text = fileToRead;
+                    }
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error: Could not read file from disk. Original error: ");
+                    }
+                }
+
+                StreamReader reader = new StreamReader(@fileToRead);
+
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                List<string> listC = new List<string>();
+                List<string> listD = new List<string>();
+                List<string> listE = new List<string>();
+                List<string> listF = new List<string>();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    listA.Add(values[0]);
+                    listB.Add(values[1]);
+                    listC.Add(values[2]);
+                    listD.Add(values[3]);
+                    listE.Add(values[4]);
+                    listF.Add(values[5]);
+                }
+
+                DataTable dtLists = new DataTable("Lists");
+
+                // Add columns to the DataTable.    
+                dtLists.Columns.Add("GUID");
+                dtLists.Columns.Add("Cast Unit Pos");
+                dtLists.Columns.Add("Fabrication Code");
+                dtLists.Columns.Add("COG X");
+                dtLists.Columns.Add("COG Y");
+                dtLists.Columns.Add("COG Z");
+
+                for (int i = 0; i < listA.Count; i++)
+                {
+                    DataRow dataRow = dtLists.NewRow();
+                    dataRow["GUID"] = listA[i];
+                    dataRow["Cast Unit Pos"] = listB[i];
+                    dataRow["Fabrication Code"] = listC[i];
+                    dataRow["COG X"] = listD[i];
+                    dataRow["COG Y"] = listE[i];
+                    dataRow["COG Z"] = listF[i];
+
+                    dtLists.Rows.Add(dataRow);
+                }
+                dataTable = dtLists;
+                dataGridView1.DataSource = dataTable;
+
+                int rowsCount = dataTable.Rows.Count;
+
+                textBox2.Text = rowsCount + " " + "COGs will be created";
+            }
+
+            else
+            {
+                MessageBox.Show("Model not open or version incompatible!", "COG Message");
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Model MyModel = new Model();
+            TSM.Beam newObject = new TSM.Beam();
+
+            int rowsCount = dataTable.Rows.Count;
+            //string fabCode = "";
+
+            if (rowsCount == 0)
+            {
+                MessageBox.Show("Open the file to proceed!", "COG Message");
+            }
+            else
+            {
+                progressBar1.Minimum = 1;
+                progressBar1.Maximum = rowsCount;
+                progressBar1.Value = 1;
+                progressBar1.Step = 1;
+
+                for (int i = 0; i < rowsCount; i++)
+                {
+                    newObject.Name = "COG for: " + dataTable.Rows[i]["Fabrication Code"].ToString();
+                    //fabCode = newObject.Name;
+                    //Console.WriteLine(newObject.Name + " "  + fabCode);
+                    //newObject.SetUserProperty("FABRICATION_CODE", fabCode);
+                    newObject.Profile.ProfileString = "ELD1*1*100*100";//ELD1*15*100*100 d100
+                    newObject.Material.MaterialString = "Z35";
+                    newObject.Class = "1001";
+                    newObject.PartNumber.Prefix = "cog";
+                    newObject.AssemblyNumber.Prefix = "COG";
+                    newObject.StartPoint.X = Convert.ToDouble(dataTable.Rows[i]["COG X"]);
+                    newObject.StartPoint.Y = Convert.ToDouble(dataTable.Rows[i]["COG Y"]);
+                    newObject.StartPoint.Z = Convert.ToDouble(dataTable.Rows[i]["COG Z"]);
+                    newObject.EndPoint.X = Convert.ToDouble(dataTable.Rows[i]["COG X"]);
+                    newObject.EndPoint.Y = Convert.ToDouble(dataTable.Rows[i]["COG Y"]);
+                    newObject.EndPoint.Z = Convert.ToDouble(dataTable.Rows[i]["COG Z"]) + 100;
+                    newObject.Position.Rotation = Position.RotationEnum.TOP;
+                    newObject.Position.Plane = Position.PlaneEnum.MIDDLE;
+                    newObject.Position.Depth = Position.DepthEnum.MIDDLE;
+
+                    newObject.Insert();
+                    progressBar1.PerformStep();
+                    textBox2.Text = rowsCount + " " + "COGs created";
+                }
+                MyModel.CommitChanges();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
